c32b0a27b97ee38af7720046842c767499ab7566
rosto-infinity <rostoinfocus@gmail.com>
2022-06-27 04:25:44 +0100

    public/index.php

--- public/index.php
+++ public/index.php
@@ -1,4 +1,17 @@
 <?php
 
+use Router\Router;
+
+require_once"../vendor/autoload.php";
+
+// echo "test1";
 // echo $_GET['url'];
-echo "salut";
+
+$router = new Router($_GET['url']);
+
+// $router->get( '/', 'App\controllers\BlogController@index');
+$router->get( '/', 'App\controllers\BlogController@welcome');
+$router->get( '/posts/', 'App\controllers\BlogController@index');
+$router->get( '/posts/:id', 'App\controllers\BlogController@show');
+
+$router->rum();
